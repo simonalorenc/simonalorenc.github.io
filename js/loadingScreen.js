06cc92a9f8fec1d823de244b2b20e090c05c6617
maciek-s <maciek@gmail.com>
2019-04-05 22:52:49 +0200

    new photos

--- js/loadingScreen.js
+++ js/loadingScreen.js
@@ -11,16 +11,26 @@ loadBar.set(10);
 function showPage() {
     loadBar.set(100);
     setTimeout(slideDelay, 1000);
-    //    document.getElementById("loader").style.display = "none";
-    
-    //  document.getElementById("myDiv").style.display = "block";
 }
 
 function slideDelay() {
     $('.load-screen').addClass("slideOutDown");
+    setTimeout(closeIfAnimationUnsupported, 900);
 }
+
+function closeIfAnimationUnsupported() {
+    $('#loader').fadeOut("slow");
+    setTimeout(closeIfAnimationUnsupportedForce, 400);
+
+}
+
+function closeIfAnimationUnsupportedForce() {
+    $('#loader').css("z-index", -1);
+}
+
+
 
 window.onload = function () {
     loadBar.set(60);
-    setTimeout(showPage, 1000);
+    setTimeout(showPage, 500);
 }
